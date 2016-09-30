fb28403e3f9d6193dd0d558bf7f5cda75aeeaeef
Arun Mallya - peach <arun.mallya@gmail.com>
2016-09-30 16:58:56 -0500

    Add load_cnn function, paths are currently fixed

--- net_utils.lua
+++ net_utils.lua
@@ -6,6 +6,42 @@ require 'loadcaffe'
 
 local net_utils = {}
 
+--[[
+Load a cnn using loadcaffe.
+Inputs:
+- name: CNN name
+- backend: loadcaffe backend to use - nn, cudnn, etc.
+- path_offset: path to cnn model
+--]]
+function net_utils.load_cnn(name, backend, path_offset)
+  local model_dir, proto_file, model_file, layer2index = nil, nil, nil, nil
+  if name == 'fcn-32s-nyud-rgb' then
+    model_dir = 'data/models/fcn-32s-nyud'
+    proto_file = 'fcn-32s-nyud-rgb.prototxt'
+    model_file = 'fcn-32s-nyud-rgb.caffemodel'
+    -- Layer indices after relu
+    layer2index = {conv1_2=4, conv2_2=9, conv3_3=16, conv4_3=23, conv5_3=30}
+    layer_dim = {conv1_2=64, conv2_2=128, conv3_3=256, conv4_3=512, conv5_3=512, fc7=4096}
+  elseif name == 'vgg-16' then
+    model_dir = 'data/models/vgg-16'
+    proto_file = 'VGG_ILSVRC_16_layers_deploy.prototxt'
+    model_file = 'VGG_ILSVRC_16_layers.caffemodel'
+    -- Layer indices after relu
+    layer2index = {conv1_2=4, conv2_2=9, conv3_3=16, conv4_3=23, conv5_3=30}
+    layer_dim = {conv1_2=64, conv2_2=128, conv3_3=256, conv4_3=512, conv5_3=512, fc7=4096}
+  else
+    error(string.format('Unrecognized model "%s"', name))
+  end
+  if path_offset then
+    model_dir = paths.concat(path_offset, model_dir)
+  end
+  print('loading network weights from .. ' .. model_file)
+  proto_file = paths.concat(model_dir, proto_file)
+  model_file = paths.concat(model_dir, model_file)
+  local cnn = loadcaffe.load(proto_file, model_file, backend)
+  return {cnn, layer2index, layer_dim}
+end
+
 
 function net_utils.subsequence(net, start_idx, end_idx)
 --[[ Get a subsequence of a Sequential network.
